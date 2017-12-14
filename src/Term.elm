80c39fd3fc3abb85fd9ff2a491021813ef4d6da3
Max Mantel <mantel.max@student.uni-halle.de>
2017-12-14 22:28:38 +0100

    decoders

--- src/Term.elm
+++ src/Term.elm
@@ -1,5 +1,9 @@
 module Term exposing (..)
 
+import Json.Decode exposing (Decoder, string, int, list, map, map2, map3, field, keyValuePairs)
+import Dict
+import Tuple
+
 type alias Term =
     { id : Int
     , name : String
@@ -10,9 +14,19 @@ type alias TermSorting =
     List { id : Int, relevance : Int}
 
 type alias GetTermsResult =
-    { topic : [(String, TermSorting)]
-    , terms : Dict String Term
+    { topic : (Int, TermSorting)
+    , terms : Dict Int Term
     }
+
+-- decoder
+intDictDecoder : Decoder a -> Decoder (Dict Int a)
+intDictDecoder decoder =
+    map
+        Dict.fromList
+        (map
+            (List.map (Tuple.mapFirst toInt))
+            (keyValuePairs decoder)
+        )
 
 termDecoder : Decoder Term
 termDecoder =
@@ -32,5 +46,13 @@ termSortingDecoder =
 getTermsDecoder : Decoder GetTermsResult
 getTermsDecoder =
     map2 GetTermsResult
-        (field "Topic" (keyValuePairs (field "Top_Terms" termSortingDecoder)))
-        (field "Term" (dict termDecoder))
+        (field "Topic"
+            (map
+                (Tuple.mapFirst toInt)
+                (map
+                    (Maybe.withDefault (0, {id = 0, relevance = 0}) List.head)
+                    (keyValuePairs (field "Top_Terms" termSortingDecoder))
+                )
+            )
+        )
+        (field "Term" (intDictDecoder termDecoder))
--- src/Topic.elm
+++ src/Topic.elm
@@ -1,122 +1,52 @@
 module Topic exposing (..)
 
-import Term
+import Term exposing (..)
 
-import Json.Decode exposing (Decoder, string, int, list, map3, field, succeed)
-import Json.Decode.Helper exposing (pseudolist, decodeValue)
+import Json.Decode exposing (Decoder, string, int, list, map, map4, field, keyValuePairs)
 
 type alias Topic =
     { id : Int
-    , hirarchical_topic :
-        { start : Int
-        , end : Int
-        , depth : Int
-        , cluster : String
-        }
+    , hirarchical_topic : TopicHirarchie
     , color_topic : String
     , top_terms : TermSorting
     }
 
--- Setter
-setTopicId : Topic -> Int -> Topic
-setTopicId topic value = { topic | id = value}
+type alias TopicHirarchie =
+    { start : Int
+    , end : Int
+    , depth : Int
+    , cluster : String
+    }
 
-setHirarchicalTopicStart : Topic -> Int -> Topic
-setHirarchicalTopicStart topic value =
-    let new_hirarchical_topic = { topic.hirarchical_topic | start = value}
-    in
-    { topic | hirarchical_topic = new_hirarchical_topic}
-
-setHirarchicalTopicEnd : Topic -> Int -> Topic
-setHirarchicalTopicEnd topic value =
-    let new_hirarchical_topic = { topic.hirarchical_topic | end= value}
-    in
-    { topic | hirarchical_topic = new_hirarchical_topic}
-
-setHirarchicalTopicDepth : Topic -> Int -> Topic
-setHirarchicalTopicDepth topic value =
-    let new_hirarchical_topic = { topic.hirarchical_topic | depth = value}
-    in
-    { topic | hirarchical_topic = new_hirarchical_topic}
-
-setHirarchicalTopicCluster : Topic -> String -> Topic
-setHirarchicalTopicCluster topic value =
-    let new_hirarchical_topic = { topic.hirarchical_topic | cluster = value}
-    in
-    { topic | hirarchical_topic = new_hirarchical_topic}
-
-setColorTopicColor : Topic -> String -> Topic
-setTopicId topic value = { topic | color_topic = value}
-
-setTopicTopTerms : Topic -> TermSorting -> Topic
-setTopicTopTerms topic value = { topic | top_terms = value}
-
--- raw topics data
-type alias Topics =
+type alias TopicResult =
     { topics : List Topic
     , sorting : List Int
     , terms : List Term
     , topicsBestItemLimit : Int
     }
 
-setTopics : Topics -> List Topic -> Topcis
-setTopics topicsa value = { topicsa | topics = value}
-
-setSorting : Topics -> List Int -> Topcis
-setSorting topicsa value = { topicsa | sorting = value}
-
-setTopics : Topics -> List Term -> Topcis
-setTopics topicsa value = { topics | terms = value}
-
-setTopicsBestItem : Topics -> Int -> Topcis
-setTopicsBestItem topicsa value = { topicsa | topicsBestItemLimit = value}
-
 -- decoder
+pseudolist : Decoder a -> Decoder (List a)
+pseudolist decoder =
+    map Tupple.second (keyValuePairs decoder)
+
 topicDecoder : Decoder Topic
 topicDecoder =
-    let
-        convert : Value -> Result Topic
-        convert value =
-            succeed
-                { id = 0
-                , hirarchical_topic =
-                    { start = 0
-                    , end = 0
-                    , depth = 0
-                    , cluster = ""
-                    }
-                , color_topic : { color = ""}
-                , top_terms : [{ id = 0, relevance = 0}]
-                }
-                |> decodeField "topic_id" int setTopicId value
-                |> decodeField "hirarchical_topic$start" int setHirarchicalTopicStart value
-                |> decodeField "hirarchical_topic$end" int setHirarchicalTopicEnd value
-                |> decodeField "hirarchical_topic$depth" int setHirarchicalTopicDepth value
-                |> decodeField "hirarchical_topic$cluster" string setHirarchicalTopicCluster value
-                |> decodeField "color_topic$color" string setColorTopicColor value
-                |> decodeField "top_terms"
-                    (list
-                        (map2 TermSorting
-                            (field "TermId" int)
-                            (field "relevance" int)
-                    ))
-                    setTopicTopTerms value
-    in
-        string |> andThen convert
+    map4 Topic
+        (field "topic_id" int)
+        (map4 TopicHirarchie
+            (field "hirarchical_topic$start" int)
+            (field "hirarchical_topic$end" int)
+            (field "hirarchical_topic$depth" int)
+            (field "hirarchical_topic$cluster" string)
+        )
+        (field "color_topic$color" string)
+        (field "top_terms" termSortingDecoder)
 
-decodeTopics : Decoder Topic
+decodeTopics : Decoder TopicResult
 decodeTopics =
-    let convert : Value -> Result Topic
-        convert value =
-            succeed
-                { topic = []
-                , topic_sorting = []
-                , terms = []
-                , topicBestItemLimit = 0
-                }
-                |> decodeField "Topic" (pseudolist decodeTopic) setTopics value
-                |> decodeField "TOPIC_SORTING" (list int) setSorting value
-                |> decodeField "Term" (pseudolist termDecoder) setTerms value
-                |> decodeField "TopicBestItemLimit" int setTopicsBestItem value
-    in
-        string |> andThen convert
+    map4 TopicResult
+        (field "Topic" (pseudolist topicDecoder)
+        (field "TOPIC_SORTING" (list int))
+        (field "Term" (pseudolist termDecoder)
+        (field "TopicBestItemLimit" int)
