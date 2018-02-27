e88efeba27f967bd8db6ca6174c32b6ecfcc18f2
Max Mantel <mantel.max@student.uni-halle.de>
2018-02-27 18:13:58 +0100

    alternative int Decoder

--- src/Document.elm
+++ src/Document.elm
@@ -101,7 +101,7 @@ bestDocsDecoder =
                         (field "KEYWORD_TITLE" string)
                         (field "TOP_TOPIC" (list int))
                         (field "LINK$URL" string)
-                        (field "TIME$TIME_STAMP" int)
+                        (field "TIME$TIME_STAMP" int2)
                         (field "TEXT$TITLE" string)
                         (field "TEXT$SNIPPET" string)
                     )
